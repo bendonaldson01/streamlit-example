f103a24e78db62600e01842157b5627b7da7e5c6
Ben Donaldson <52375867+bendonaldson01@users.noreply.github.com>
2021-10-20 20:09:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+
 """
 # My first app
 Here's our first attempt at using data to create a table:
