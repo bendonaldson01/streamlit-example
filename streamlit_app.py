4439ef6a27422e7da9dee74b74f8127f6daf6ad9
Ben Donaldson <52375867+bendonaldson01@users.noreply.github.com>
2021-10-20 20:12:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ import math
 import pandas as pd
 import streamlit as st
 
-st.table("Here's our first attempt at using data to create a table:")
-st.table(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
