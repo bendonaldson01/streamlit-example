ce9a92094f701f3cd875cee108f7f87cda99ea8e
Ben Donaldson <52375867+bendonaldson01@users.noreply.github.com>
2021-10-21 19:13:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,13 +5,6 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 
-
-st.radio(
-        'Sorting hat',
-        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
-    st.write(f"You are in {chosen} house!")
- 
-chart_data = pd.DataFrame(
+pd.DataFrame(
      np.random.randn(20, 3),
      columns=['a', 'b', 'c'])
-st.line_chart(chart_data)
