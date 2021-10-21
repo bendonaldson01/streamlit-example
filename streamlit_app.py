d4aed7e3ab63ddbe787e9a87b4c1e04d6ad97757
Ben Donaldson <52375867+bendonaldson01@users.noreply.github.com>
2021-10-21 19:12:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,16 +5,13 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 
-left_column, right_column = st.columns(2)
-# You can use a column just like st.sidebar:
-with left_column.button(chart_data = pd.DataFrame(
+
+st.radio(
+        'Sorting hat',
+        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+st.write(f"You are in {chosen} house!")
+ 
+chart_data = pd.DataFrame(
      np.random.randn(20, 3),
      columns=['a', 'b', 'c'])
 st.line_chart(chart_data)
-)
-# Or even better, call Streamlit functions inside a "with" block:
-with right_column:
-    chosen = st.radio(
-        'Sorting hat',
-        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
-st.write(f"You are in {chosen} house!") 
